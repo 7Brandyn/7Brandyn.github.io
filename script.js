90433857894717abe4dca5ae55367ed1f18598ad
7Brandyn <brandyn.evans7@gmail.com>
2022-03-31 13:47:54 -0700

    added a few images, commented out music player until its finished

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() { startscript(); }, false);
+var player;
+
+function startscript() 
+{
+ player = document.getElementById('music_player');
+ player.controls = false;
+}
+
+function play_aud() 
+{
+ player.play();
+} 
+function pause_aud() 
+{
+ player.pause();
+}
+function stop_aud() 
+{
+ player.pause();
+ player.currentTime = 0;
+}
+function change_vol()
+{
+ player.volume=document.getElementById("change_vol").value;
+}

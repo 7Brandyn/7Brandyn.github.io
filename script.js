251efc796ea07327186c4d8953f4d592bf0cbf4f
7Brandyn <brandyn.evans7@gmail.com>
2022-04-07 21:24:03 -0700

    Added dark & lite themes to portfolio

--- script.js
+++ script.js
@@ -25,3 +25,17 @@ function change_vol()
  player.volume=document.getElementById("change_vol").value;
 }
 */
+
+function darkMode() {
+    var element = document.body;
+    var content = document.getElementById("DarkModetext");
+    element.className = "dark-mode";
+    content.innerText = "Dark Mode is ON";
+}
+
+function lightMode() {
+    var element = document.body;
+    var content = document.getElementById("DarkModetext");
+    element.className = "light-mode";
+    content.innerText = "Dark Mode is OFF";
+}

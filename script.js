10e1b90b9bdadf4bf46a4d1d7e25db82afd870ee
7Brandyn <brandyn.evans7@gmail.com>
2022-05-07 08:14:55 -0700

    I've added javascript games including a few audio files and images that will enhance the games features a bit. I also created a GAMES link in the navbar and created a GAMES html page.

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function nightMode() {
-    var element = document.body;
-    var content = document.getElementById("NightModetext");
+    let element = document.body;
+    let content = document.getElementById("NightModetext");
     element.className = "night-mode";
     content.innerText = "Night Mode is ON";
 }
 
 function dayMode() {
-    var element = document.body;
-    var content = document.getElementById("NightModetext");
+    let element = document.body;
+    let content = document.getElementById("NightModetext");
     element.className = "day-mode";
     content.innerText = "Night Mode is OFF";
 }
@@ -19,6 +19,7 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 })
+
 
 /*document.addEventListener("DOMContentLoaded", function() { startscript(); }, false);
 var player;

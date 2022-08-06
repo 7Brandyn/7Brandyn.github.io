825345efbf51938379dcbc66220db624f9beaed2
7Brandyn <brandyn.evans7@gmail.com>
2022-08-06 00:19:40 -0700

    Removed PHP and made a simpler contact form using JS instead.

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function openNav() {
     document.getElementById("myNavbar").style.width = "0%";
 }
 
-document.querySelectorAll('a[href^=""]').forEach(anchor => {
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
 
@@ -15,3 +15,34 @@ document.querySelectorAll('a[href^=""]').forEach(anchor => {
         });
     });
 });
+
+var form = document.getElementById("my-form");
+    
+        async function handleSubmit(event) {
+          event.preventDefault();
+          var status = document.getElementById("my-form-status");
+          var data = new FormData(event.target);
+          fetch(event.target.action, {
+            method: form.method,
+            body: data,
+            headers: {
+                'Accept': 'application/json'
+            }
+          }).then(response => {
+            if (response.ok) {
+              status.innerHTML = "Your message went through!";
+              form.reset()
+            } else {
+              response.json().then(data => {
+                if (Object.hasOwn(data, 'errors')) {
+                  status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+                } else {
+                  status.innerHTML = "Oops! There was a problem..."
+                }
+              })
+            }
+          }).catch(error => {
+            status.innerHTML = "Oops! There was a problem submitting your form"
+          });
+        }
+        form.addEventListener("submit", handleSubmit)

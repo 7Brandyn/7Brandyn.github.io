8b73b4d0315a76263ec41632eecfd64bff2df5fc
7Brandyn <brandyn.evans7@gmail.com>
2022-10-21 16:06:13 -0700

    Created New Portfolio

--- script.js
+++ script.js
@@ -1,48 +1,8 @@
-function openNav() {
-    document.getElementById("myNavbar").style.width = "100%";
-}
-  
-  function closeNav() {
-    document.getElementById("myNavbar").style.width = "0%";
-}
-
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
-
-var form = document.getElementById("my-form");
-    
-    async function handleSubmit(event) {
-        event.preventDefault();
-        var status = document.getElementById("my-form-status");
-        var data = new FormData(event.target);
-        fetch(event.target.action, {
-        method: form.method,
-        body: data,
-        headers: {
-            'Accept': 'application/json'
-        }
-        }).then(response => {
-        if (response.ok) {
-            status.innerHTML = "Your message went through!";
-            form.reset()
-        } else {
-            response.json().then(data => {
-            if (Object.hasOwn(data, 'errors')) {
-                status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
-            } else {
-                status.innerHTML = "Oops! There was a problem..."
-            }
-            })
-        }
-        }).catch(error => {
-        status.innerHTML = "Oops! There was a problem submitting your form"
-        });
+function navSelector() {
+    var n = document.getElementById("myTopnav");
+    if (n.className === "topnav") {
+        n.className += " responsive";
+    } else {
+        n.className = "topnav";
     }
-    form.addEventListener("submit", handleSubmit)
+}
